11baec82269c961fe3eda7c9f53da32bda883bc4
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-02 10:01:56 -0400

    Adding comments to FutureValue Calculator

--- src/main/java/com/pluralsight/FutureValue.java
+++ src/main/java/com/pluralsight/FutureValue.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
+//This is calculating future value of CD
 public class FutureValue {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
@@ -14,30 +15,35 @@ public class FutureValue {
         System.out.printf("CD ending balance after %d years will be %.2f", numberOfTimesMoneyInvestedInYear, cdEndingBalance);
     }
 
+    //This method is getting users principle amount
     public static int userPrinciple(){
         System.out.print("What is your principle amount: ");
         int principle = scanner.nextInt();
         return principle;
     }
 
+    //This method is getting user interest rate.
     public static double userAnnualInterestRate(){
         System.out.print("What is your interest rate: ");
         double interestRate = scanner.nextDouble();
         return interestRate;
     }
 
+    //This method is getting compounded interest per year
     public static int userNumberOfTimesInterestCompoundedPerYear(){
         System.out.print("How many times does your interest compound per year: ");
         int timesCompoundedPerYear = scanner.nextInt();
         return timesCompoundedPerYear;
     }
 
+    //This method is getting how long the users wants to invest for
     public static int userNumberOfTimesMoneyInvestedInYear(){
         System.out.print("How many years did you invest your money for: ");
         int timeInYear = scanner.nextInt();
         return timeInYear;
     }
 
+    //This method is calculating how much the user will have at the end of their investing period. 
     public static double userCDEndingBalance(int principle, double annualInterestRate, int numberOfTimesMoneyInvestedInYear, int numberOfTimesInterestCompoundedPerYear){
         double balance = principle * (Math.pow(1 + (annualInterestRate / 100)/numberOfTimesInterestCompoundedPerYear, numberOfTimesMoneyInvestedInYear * numberOfTimesInterestCompoundedPerYear));
         return balance;
